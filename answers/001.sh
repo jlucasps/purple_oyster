7858543ead4c0d7f71f13a40ac7944fa73c1c424
Joao Lucas Santana <jlucasps@gmail.com>
2015-06-04 12:24:58 -0300

    Add structure to folders

---
+++ answers/001.sh
@@ -0,0 +1,2 @@
+echo "scenario-001:"
+# YOUR CODE HERE
---
+++ scenarios/001.sh
@@ -0,0 +1,5 @@
+adduser --disabled-password --gecos "" tome
+adduser --disabled-password --home /users/gabriel --shell /bin/bash --gecos "" gabriel
+adduser --disabled-password --gecos "" maria
+adduser --disabled-password --home /people/claudia --shell /bin/bash --gecos "" claudia
+adduser --disabled-password --gecos "" bibiana
