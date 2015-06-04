ce5b87fd83d159533200dc11b5d5e8588bb0dace
Gustavo Freitas <gustavof@gmail.com>
2015-06-04 12:04:32 -0300

    Creating users using the shell script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,2 +1,8 @@
 apt-get update
 apt-get install -y git bash vim curl tree
+
+adduser --disabled-password --gecos "" tome
+adduser --disabled-password --home /users/gabriel --shell /bin/bash --gecos "" gabriel
+adduser --disabled-password --gecos "" maria
+adduser --disabled-password --home /people/claudia --shell /bin/bash --gecos "" claudia
+adduser --disabled-password --gecos "" bibiana
